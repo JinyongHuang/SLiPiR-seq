1652d0283ffc847d9244ed0e392e6f60c1804f82
JinyongHuang <99039278+JinyongHuang@users.noreply.github.com>
2023-09-14 15:41:33 +0800

    Update 2. goi_reads_boxplot.R

--- Data analysis/2. Lung cancer/2. Machine learning/2. goi_reads_boxplot.R
+++ Data analysis/2. Lung cancer/2. Machine learning/2. goi_reads_boxplot.R
@@ -15,19 +15,20 @@ AllRNA<-read.table("../../../Normalization_RPM_colsum/cfRNA-log2(cpm).txt", head
 AllRNA<-AllRNA[,meta$ID]
 
 ##GOI
-goi_msRNA<-readLines("../LASSO/goi/20/msRNA name.txt")
-goi_miRNA<-readLines("../Boruta/goi/20/miRNA name.txt")
-goi_snRNA<-readLines("../LASSO/goi/20/snRNA name.txt")
-goi_snoRNA<-readLines("../TopN/goi/snoRNA Top N selected name.txt")
-goi_tsRNA<-readLines("../LASSO/goi/20/tsRNA name.txt")
-goi_list<-list(msRNA=goi_msRNA,miRNA=goi_miRNA,
+goi_mRNA<-readLines("../LASSO_10/goi/mRNA name.txt")
+goi_miRNA<-readLines("../LASSO_10/goi/miRNA name.txt")
+goi_snRNA<-readLines("../LASSO_10/goi/snRNA name.txt")
+goi_snoRNA<-readLines("../LASSO_10/goi/snoRNA name.txt")
+goi_tsRNA<-readLines("../LASSO_10/goi/tsRNA name.txt")
+goi_rsRNA<-readLines("../LASSO_10/goi/rsRNA name.txt")
+goi_ysRNA<-readLines("../LASSO_10/goi/ysRNA name.txt")
+goi_list<-list(mRNA=goi_mRNA,miRNA=goi_miRNA,
                tsRNA=goi_tsRNA,snRNA=goi_snRNA,snoRNA=goi_snoRNA,
-               cfRNA=c(goi_msRNA,goi_miRNA,goi_tsRNA,goi_snoRNA))
+               rsRNA=goi_rsRNA,ysRNA=goi_ysRNA)
 
 ####case and control----
-##each RNA
-subset<-AllRNA[GOI_lung,]
-GOI_lung<-c(goi_msRNA,goi_miRNA,goi_tsRNA,goi_snRNA,goi_snoRNA)
+##each RNAsubset<-AllRNA[GOI_lung,]
+GOI_lung<-c(goi_mRNA,goi_miRNA,goi_tsRNA,goi_snRNA,goi_snoRNA,goi_rsRNA,goi_ysRNA)
 dir.create(paste0(getwd(),"/Boxplot_each case and control"), showWarnings = FALSE)
 subset<-AllRNA[GOI_lung,]
 subset<-data.frame(t(subset),check.names = FALSE)
@@ -62,51 +63,13 @@ for (i in N) {
   ggsave(paste0("Boxplot_each case and control/",colnames(subset)[i],".pdf"),device = "pdf",width = 4, height = 4)
 }}
 
-combine<-ggarrange(plotlist[["AP1B1"]],plotlist[["FOXF1"]],plotlist[["TMC5"]],
-                   plotlist[["hsa-miR-25-3p"]],plotlist[["hsa-miR-1180-3p"]],plotlist[["hsa-miR-3960"]],
-                   plotlist[["SNORD6"]],plotlist[["SNORD25"]],plotlist[["SNORD53"]],
-                   plotlist[["tRF-36-PS5P4PW3FJHPEZE"]],plotlist[["tRF-39-18VBY9P9KHM2ORIZ"]],plotlist[["tRF-45-Z3R918VBY9PYKHM26R"]],
-                   ncol = 3, nrow = 4)
-ggsave("Combined 12.pdf", width=10.5, height=14)
-ggsave("Combined 12.png", width=10.5, height=14, units="in", bg="white")
-
-
-##cumulative RNA
-dir.create(paste0(getwd(),"/Boxplot_cumulative case and control"), showWarnings = FALSE)
-LCNOR<-meta[(meta$sample_type=="LC_SZDE" | meta$sample_type=="NOR_SZBA" | meta$sample_type=="LC_SZBU" | meta$sample_type=="NOR_SZDW"),]
-for (i in names(goi_list)) {
-  subset<-AllRNA[goi_list[[i]],LCNOR$ID]
-  subset<-data.frame(t(subset),check.names = FALSE)
-  subset<-as.data.frame(apply(subset, 1, sum))
-  colnames(subset)<-"RNA"
-  subset$Group<-LCNOR$Group
-  subset$Type<-LCNOR$sample_type
-  subset$Type<-gsub("LC_SZDE","LC-Disc",subset$Type)
-  subset$Type<-gsub("NOR_SZBA","NOR-Disc",subset$Type)
-  subset$Type<-gsub("LC_SZBU","LC-Vali",subset$Type)
-  subset$Type<-gsub("NOR_SZDW","NOR-Vali",subset$Type)
-  subset$Type<-factor(subset$Type,levels = c("LC-Disc","NOR-Disc","LC-Vali","NOR-Vali"))
-  stat.test <- subset %>%
-    t_test(RNA ~ Type, paired = FALSE, var.equal = FALSE, p.adjust.method = "none")%>%
-    add_significance("p")%>%
-    add_xy_position(fun="max")
-  stat.test<-stat.test[c(1,6),]
-  stat.test$y.position<-mean(stat.test$y.position)
-  if (i=="cfRNA") {
-    plot <- ggplot(subset, aes(x=Type, y=RNA)) + geom_boxplot(aes(fill = Group),outlier.shape=NA)+ scale_fill_manual(values=c("#4E62AB","#D6404E"))+ 
-      geom_jitter(shape=16, position=position_jitter(0.2), alpha=0.8)+
-      labs(title=paste0(i),x="", y = expression('Cumulative log'[2]*'(RPM)'))+ theme_minimal()+
-      theme(axis.title = element_text(size = 14), axis.text = element_text(colour = "black",size = 12),
-            plot.title = element_text(hjust=0.5,face="bold"),legend.position = "none")}
-  if (i!="cfRNA") {
-    plot <- ggplot(subset, aes(x=Type, y=RNA)) + geom_boxplot(aes(fill = Group),outlier.shape=NA)+ scale_fill_manual(values=c("#4E62AB","#D6404E"))+ 
-      geom_jitter(shape=16, position=position_jitter(0.2), alpha=0.8)+
-      labs(title=paste0(i), x="", y = expression('Cumulative log'[2]*'(RPM)'))+ theme_minimal()+
-      theme(axis.title = element_text(size = 14), axis.text = element_text(colour = "black",size = 12), 
-            plot.title = element_text(hjust=0.5,face="bold"),legend.position = "none")}
-  plot + stat_pvalue_manual(stat.test, label = "p.signif",tip.length = 0.02,bracket.shorten = 0.1)
-  ggsave(paste0("Boxplot_cumulative case and control/",i,".png"),device = "png",bg="white",width = 4, height = 4)
-  ggsave(paste0("Boxplot_cumulative case and control/",i,".pdf"),device = "png",width = 4, height = 4)
-}
-
+combine<-ggarrange(plotlist[["AP1B1"]],plotlist[["FOXF1"]],plotlist[["RPL23A"]],plotlist[["TMC5"]],
+                   plotlist[["hsa-miR-185-5p"]],plotlist[["hsa-miR-1180-3p"]],plotlist[["hsa-miR-3960"]],
+                   plotlist[["SNORD16"]],plotlist[["SNORD22"]],plotlist[["SNORD53"]],
+                   plotlist[["tRF-38-HMI8W47W1R7HFEV"]],plotlist[["tRF-33-R008R959KUMK9"]],plotlist[["tRF-23-BZBZOS4YV"]],
+                   plotlist[["RNU4-46P"]],plotlist[["Homo-28S-2571"]],
+                   ncol = 3, nrow = 5)
+ggsave("Combined 15.pdf", width=10.5, height=18)
+ggsave("Combined 15.png", width=10.5, height=18, units="in", bg="white")
+#ggsave("Combined 15.tiff", width=10.5, height=18, units="in", bg="white")
 
